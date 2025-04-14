05fbe59ec40693148c04e6cc6c009e3e6a8b6f17
Nikita Grechanikov <roman0810.r@gmail.com>
2025-04-14 20:01:48 +0300

    way to augmentation

---
+++ models.py
@@ -0,0 +1,288 @@
+import torch
+import torch.nn as nn
+import numpy as np
+
+class Spine(nn.Module):
+    def __init__(self, vgg):
+        super(Spine, self).__init__()
+
+        for i, child in enumerate(vgg.children()):
+            for param in child.parameters():
+                param.requires_grad = False
+
+            if i == 0:
+                layers_list = child
+
+        self.conv = nn.Sequential(*layers_list[:-1])
+
+    def forward(self, x):
+
+        x = self.conv(x)
+
+        return x
+
+
+DETECTION_CLASSES = 6
+# !!!! класс с индексом 0 - задний фон !!!!
+# Классификатор
+class СlassifierHead(nn.Module):
+    def __init__(self):
+        super(СlassifierHead, self).__init__()
+
+        self.flatten = nn.Flatten()
+        self.fc = nn.Sequential(
+            nn.Linear(in_features = 8192, out_features = 2048, bias=True),
+            nn.BatchNorm1d(2048, momentum = 0.8),
+            nn.LeakyReLU(),
+            nn.Dropout(p=0.5),
+            nn.Linear(in_features = 2048, out_features = 2048, bias=True),
+            nn.BatchNorm1d(2048, momentum = 0.8),
+            nn.LeakyReLU(),
+            nn.Dropout(p=0.5),
+            nn.Linear(in_features = 2048, out_features = 1024, bias=True),
+            nn.BatchNorm1d(1024, momentum = 0.8),
+            nn.LeakyReLU(),
+            nn.Dropout(p=0.5),
+            nn.Linear(in_features = 1024, out_features = 512, bias=True),
+            nn.BatchNorm1d(512, momentum = 0.8),
+            nn.LeakyReLU(),
+            nn.Dropout(p=0.5),
+            nn.Linear(in_features = 512, out_features = DETECTION_CLASSES + 1, bias=True),
+            nn.Softmax(dim=1)
+        )
+
+    def forward(self, x):
+        x = self.flatten(x)
+        x = self.fc(x)
+
+        return x
+
+class ClsTrainer(СlassifierHead):
+    def __init__(self,
+                lr,
+                criterion):
+        super(ClsTrainer, self).__init__()
+
+        self.lr = lr
+        self.optim = torch.optim.Adam(super().parameters(), lr=self.lr)
+        self.criterion = criterion
+        self.epo_train_losses = []
+        self.epo_val_losses = []
+
+        self.train_losses = []
+        self.val_losses = []
+
+    def train_step(self, batch, target):
+        self.train()
+        self.optim.zero_grad()
+
+        output = self.forward(batch)
+        loss = self.criterion(output, target)
+
+        loss.backward()
+        self.optim.step()
+
+        self.epo_train_losses.append(loss.item())
+
+    def eval_step(self, batch, target):
+        output = self.query(batch)
+        loss = self.criterion(output, target)
+        self.epo_val_losses.append(loss.item())
+
+    def query(self, batch):
+        self.eval()
+        with torch.no_grad():
+            output = self.forward(batch)
+
+        return output
+
+    def epoch_end(self):
+        self.train_losses.append(np.mean(self.epo_train_losses))
+        self.val_losses.append(np.mean(self.epo_val_losses))
+
+        self.epo_train_losses = []
+        self.epo_val_losses = []
+
+
+# Регрессор
+class RegressorHead(nn.Module):
+    def __init__(self):
+        super(RegressorHead, self).__init__()
+
+        self.flatten = nn.Flatten()
+
+        self.fc1 = nn.Sequential(
+            nn.Linear(in_features = 8192, out_features = 1024, bias=True),
+            nn.BatchNorm1d(1024, momentum = 0.8),
+            nn.LeakyReLU(),
+            nn.Dropout(p=0.5)
+        )
+        self.fc2 = nn.Sequential(
+            nn.Linear(in_features = 1024+4, out_features = 512, bias=True),
+            nn.BatchNorm1d(512, momentum = 0.8),
+            nn.LeakyReLU(),
+            nn.Dropout(p=0.5),
+
+            nn.Linear(in_features = 512, out_features = 256, bias=True),
+            nn.BatchNorm1d(256, momentum = 0.8),
+            nn.LeakyReLU(),
+            nn.Dropout(p=0.5),
+
+            nn.Linear(in_features = 256, out_features = 4, bias=True)
+        )
+
+    def forward(self, sample, P):
+        x = self.flatten(sample)
+        x = self.fc1(x)
+        x = torch.cat((x, P), dim=-1)
+        x = self.fc2(x)
+
+        return x
+
+
+class RgsTrainer(RegressorHead):
+    def __init__(self,
+                lr,
+                criterion):
+        super(RgsTrainer, self).__init__()
+
+        self.lr = lr
+        self.optim = torch.optim.Adam(super().parameters(), lr=self.lr)
+        self.criterion = criterion
+        self.epo_train_losses = []
+        self.epo_val_losses = []
+
+        self.train_losses = []
+        self.val_losses = []
+
+    def train_step(self, batch, target):
+        self.train()
+        self.optim.zero_grad()
+
+        input1, input2 = batch
+        output = self.forward(input1, input2)
+        loss = self.criterion(output, target)
+
+        loss.backward()
+        self.optim.step()
+
+        self.epo_train_losses.append(loss.item())
+
+    def eval_step(self, batch, target):
+        output = self.query(batch)
+        loss = self.criterion(output, target)
+        self.epo_val_losses.append(loss.item())
+
+    def query(self, batch):
+        self.eval()
+        with torch.no_grad():
+            input1, input2 = batch
+            output = self.forward(input1, input2)
+
+        return output
+
+    def epoch_end(self):
+        self.train_losses.append(np.mean(self.epo_train_losses))
+        self.val_losses.append(np.mean(self.epo_val_losses))
+
+        self.epo_train_losses = []
+        self.epo_val_losses = []
+
+
+# Сегментатор
+class SegmentHead(nn.Module):
+    def __init__(self):
+        super(SegmentHead, self).__init__()
+        self.relu = nn.LeakyReLU()
+        self.sigmoid = nn.Sigmoid()
+
+        self.conv0 = self.set_conv_block(512, 256)
+
+        self.conv1 = nn.ConvTranspose2d(256, 128, kernel_size=3, stride=2)
+        self.BN1 = nn.BatchNorm2d(128, momentum = 0.8)
+
+        self.conv2 = self.set_conv_block(128, 64)
+
+        self.conv3 = nn.ConvTranspose2d(64, 32, kernel_size=3, stride=2)
+        self.BN3 = nn.BatchNorm2d(32, momentum = 0.8)
+
+        self.conv4 = nn.Conv2d(32, DETECTION_CLASSES, kernel_size = 4, padding=0)
+
+
+
+    def forward(self, x):
+        x = self.conv0(x)
+
+        x = self.conv1(x)
+        x = self.BN1(x)
+        x = self.relu(x)
+
+        x = self.conv2(x)
+
+        x = self.conv3(x)
+        x = self.BN3(x)
+        x = self.relu(x)
+
+        x = self.conv4(x)
+        x = self.sigmoid(x)
+
+        return x
+
+    @staticmethod
+    def set_conv_block(input_size, output_size, kernel_size = 3, padding = 1):
+        block = nn.Sequential(
+            nn.Conv2d(input_size, output_size, kernel_size, padding=padding),
+            nn.BatchNorm2d(output_size, momentum=0.8),
+            nn.LeakyReLU(),
+            nn.Conv2d(output_size, output_size, kernel_size, padding=padding),
+            nn.BatchNorm2d(output_size, momentum=0.8),
+            nn.LeakyReLU()
+        )
+        return block
+
+
+class SgmTrainer(SegmentHead):
+    def __init__(self,
+                lr,
+                criterion):
+        super(SgmTrainer, self).__init__()
+
+        self.lr = lr
+        self.optim = torch.optim.Adam(super().parameters(), lr=self.lr)
+        self.criterion = criterion
+        self.epo_train_losses = []
+        self.epo_val_losses = []
+
+        self.train_losses = []
+        self.val_losses = []
+
+    def train_step(self, batch, target):
+        self.train()
+        self.optim.zero_grad()
+
+        output = self.forward(batch)
+        loss = self.criterion(output, target)
+
+        loss.backward()
+        self.optim.step()
+
+        self.epo_train_losses.append(loss.item())
+
+    def eval_step(self, batch, target):
+        output = self.query(batch)
+        loss = self.criterion(output, target)
+        self.epo_val_losses.append(loss.item())
+
+    def query(self, batch):
+        self.eval()
+        with torch.no_grad():
+            output = self.forward(batch)
+
+        return output
+
+    def epoch_end(self):
+        self.train_losses.append(np.mean(self.epo_train_losses))
+        self.val_losses.append(np.mean(self.epo_val_losses))
+
+        self.epo_train_losses = []
+        self.epo_val_losses = []
--- utils.py
+++ utils.py
@@ -1,110 +1,47 @@
-from MLtools import ImageData
-import torch
-import xml.etree.ElementTree as ET
 import numpy as np
-from typing import List
-
-import random
-from torchvision import transforms
-from torchvision.utils import save_image
-from tqdm.notebook import tqdm
-
-def add_annotation_to_xml(file_path, id, name, label, RLE, left=0, top=0, 
-                          width=640, height=640, mask_width=640, mask_height=640):
-    # Парсим XML-файл
-    tree = ET.parse(file_path)
-    root = tree.getroot()
-
-    # Создаем новый элемент <image>
-    image = ET.Element('image', {
-        'id': str(id),
-        'name': name,
-        'width': str(width),
-        'height': str(height)
-    })
-    
-    str_RLE = lsit_to_string(RLE)
-    # Создаем новый элемент <mask> и добавляем его в <image>
-    mask = ET.SubElement(image, 'mask', {
-        'label': label,
-        'source': "manual",
-        'occluded': "0",
-        'rle': str_RLE,
-        'left': str(left),
-        'top': str(top),
-        'width': str(mask_width),
-        'height': str(mask_height),
-        'z_order': "0"
-    })
-
-    # Добавляем новый элемент <image> в корневой элемент <annotations>
-    root.append(image)
-
-    # Сохраняем изменения в XML-файл
-    tree.write(file_path, encoding='utf-8', xml_declaration=True)
+import torch
 
 
-def rle_encode_binary(array) -> List[int]:
-    # Выравниваем массив в одномерный
-    flattened = array.flatten()
-    
-    # Инициализируем переменные для RLE
-    rle = []
-    prev_value = flattened[0]
-    count = 1
-    
-    # Проходим по массиву и строим RLE
-    for value in flattened[1:]:
-        if value == prev_value:
-            count += 1
-        else:
-            rle.append(count)
-            prev_value = value
-            count = 1
-    
-    # Добавляем последний элемент
-    rle.append(count)
-    
-    return rle
+class RLETransform(torch.nn.Module):
+    '''
+    transforms CVAT data to proper objects of labels, bboxes and masks
+    '''
+    def forward(self, Target):
+        labels = []
+        bboxes = []
+        masks = []
+
+        for data in Target.annotations.shapes:
+            # лейблы в датасете начинаются с 9. Нулевая метка занята под фон.
+            # Поэтому все метки надо сдвинуть на 8 влево
+            labels.append(data['label_id']-8)
+
+            bboxes_raw = data['points'][-4:]
+            # y1, x1, y2, x2
+            bboxes.append([bboxes_raw[1], bboxes_raw[0], bboxes_raw[3], bboxes_raw[2]])
+
+            masks.append(self.rle_to_mask(data['points'][:-4], bboxes[-1]))
+
+        return labels, bboxes, masks
 
 
-def lsit_to_string(rle_result):
-    s = ""
-    for el in rle_result:
-        s += str(el) + ', '
-        
-    return s[:-2]
+    def rle_to_mask(self, rle, bbox):
+        height = int(bbox[2]-bbox[0])+1
+        width  = int(bbox[3]-bbox[1])+1
+        top = int(bbox[0])
+        left = int(bbox[1])
+
+        mask = np.zeros(width * height, dtype=np.uint8)
+
+        rle.insert(0,0.0)
+        rle_pairs = np.array(rle, dtype=np.uint32)
+
+        pos = 0
+        for i in range(0, len(rle_pairs), 2):
+            start = pos
+            end = pos + int(rle_pairs[i])
+            mask[start:end] = 1  # Устанавливаем пиксели в белый цвет (255)
+            pos = end + int(rle_pairs[i + 1])  # Пропускаем следующие пиксели
 
 
-def aug_dataset(Data: ImageData, n_copys: int=10, xml_path = "annotations.xml",
-               images_path="resized_fruits"):
-    transform_train = transforms.Compose([
-        transforms.ToPILImage(),
-        transforms.RandomHorizontalFlip(),
-        transforms.RandomRotation(90),
-        transforms.PILToTensor(),
-    ])
-#   добавляем новые записи с конца xml списка
-    cur_id = len(Data)
-    for index, (image, mask) in tqdm(enumerate(Data)):
-        orig_name, label = Data.get_name_label(index)
-#       кол-во копий делается на одну меньше чтобы вместе с оригиналом вышло по n_copys штук
-        for i in range(cur_id,cur_id+n_copys-1):
-            seed = random.randint(-1000, 1000)
-            name = str(i) + "_" + orig_name
-
-            torch.manual_seed(seed)
-            tr_image = transform_train(torch.tensor(image))
-#           сохраняем новое изображение в общую папку
-            save_image(tr_image.float()/255.0, f'{images_path}/{name}')
-
-            torch.manual_seed(seed)
-            tr_mask = transform_train(torch.tensor(mask))
-
-    #       переводим новую маску в строку типа RLE
-            RLE = rle_encode_binary(tr_mask/255.0)
-
-    #       добавляем новую маску в xml
-            add_annotation_to_xml(xml_path, i, name, label, RLE)
-        
-        cur_id = cur_id+n_copys-1
+        return torch.unsqueeze(torch.from_numpy(mask.reshape((height, width))), dim=0)
